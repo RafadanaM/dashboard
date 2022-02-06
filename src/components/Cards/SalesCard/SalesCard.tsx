9b17d421179193ea7974d9b77c44e5b34159a847
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2022-02-06 17:54:34 +1000

    feat: add sales card

--- src/components/Cards/SalesCard/SalesCard.tsx
+++ src/components/Cards/SalesCard/SalesCard.tsx
@@ -1,7 +1,23 @@
 import BaseCard from "../BaseCard/BaseCard";
-
+import classes from "./SalesCard.module.css";
+import { ReactComponent as Cart } from "../../../assets/images/Cart.svg";
+import { ReactComponent as Down } from "../../../assets/images/DownArrow.svg";
 const SalesCard = () => {
-  return <BaseCard title="Sales Turnover">THIS IS SALES CARD</BaseCard>;
+  return (
+    <BaseCard title="Sales Turnover" titleStyle={classes.title}>
+      <div className={classes.container}>
+        <div className={classes.detail}>
+          <span className={classes.price}>Rp 3,600,000</span>
+          <div className={classes.percentContainer}>
+            <Down className={classes.down} />
+            <span>13.8%</span>
+            <span>last period in products sold</span>
+          </div>
+        </div>
+        <Cart className={classes.cart} />
+      </div>
+    </BaseCard>
+  );
 };
 
 export default SalesCard;
