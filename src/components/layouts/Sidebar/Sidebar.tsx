0b6c8280cc2a582ae8ddcd255643bae32db18417
Muhammad Rafadana Mountheira <RafadanaM@gmail.com>
2022-02-06 17:35:31 +1000

    feat: add sidebar menu logo

--- src/components/layouts/Sidebar/Sidebar.tsx
+++ src/components/layouts/Sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import classes from "./Sidebar.module.css";
 import SidebarItem from "./SidebarItem/SidebarItem";
 import { ReactComponent as DashboardIcon } from "../../../assets/images/Dashboard_icon.svg";
+import { ReactComponent as MenuIcon } from "../../../assets/images/Menu.svg";
 const Sidebar = () => {
   return (
     <div className={classes.container}>
-      <SidebarItem icon={<DashboardIcon className={classes.icon} />} />
+      <SidebarItem icon={<MenuIcon className={classes.menu} />} />
+      <SidebarItem icon={<DashboardIcon className={classes.icon} />} selected />
     </div>
   );
 };
--- src/components/layouts/Sidebar/SidebarItem/SidebarItem.tsx
+++ src/components/layouts/Sidebar/SidebarItem/SidebarItem.tsx
@@ -2,10 +2,19 @@ import classes from "./SidebarItem.module.css";
 
 interface ISideBarItem {
   icon: any;
+  selected?: boolean | undefined;
 }
 
-const SidebarItem = ({ icon }: ISideBarItem) => {
-  return <div className={classes.container}>{icon}</div>;
+const SidebarItem = ({ icon, selected }: ISideBarItem) => {
+  return (
+    <div
+      className={`${classes.container} ${
+        selected ? classes.containerActive : ""
+      }`}
+    >
+      {icon}
+    </div>
+  );
 };
 
 export default SidebarItem;
